18f9f727fa8d16b539d29765e2a1dc7a8f2fa743
MNPPI223 <mnppi223@gmail.com>
2026-03-04 15:57:18 +0900

    금일 그룹 연습문제 푸쉬하겠습니다!

---
+++ src/a_polymorphism_groupe_practice/HumanRPG.java
@@ -0,0 +1,17 @@
+package a_polymorphism_groupe_practice;
+
+public class HumanRPG {
+
+    public void HP(){
+        System.out.println("체력을 보충합니다!");
+    }
+
+    public void go() {
+        System.out.println("마을을 돌아다닙니다!");
+    }
+
+    public void MP() {
+        System.out.println("마나를 보충합니다!");
+    }
+
+}
